4b79bb88cac2469eae48ef7db939b9512f1c6e82
Akeung <zzkeung@163.com>
2020-08-03 00:28:29 +0800

    feat: 重构common

--- provider-payment8001/src/main/java/com/ak/demo/controller/PaymentController.java
+++ provider-payment8001/src/main/java/com/ak/demo/controller/PaymentController.java
@@ -10,7 +10,6 @@ import com.ak.demo.entities.Payment;
 import com.ak.demo.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 /**
